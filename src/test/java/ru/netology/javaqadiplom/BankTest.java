6f2f24ebe84dc6a638520207b8db4101f7f8c765
Alexandra Serafimskaya <a79256103066@gmail.com>
2025-11-19 19:46:20 +0300

    fix bugs in BankTest Class

--- src/test/java/ru/netology/javaqadiplom/BankTest.java
+++ src/test/java/ru/netology/javaqadiplom/BankTest.java
@@ -1,39 +1,37 @@
 package ru.netology.javaqadiplom;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 
 public class BankTest {
 
-    Account roman = new Account();
-    Account alex = new Account();
+    CreditAccount accountRoman = new CreditAccount(
+            5000,
+            1000,
+            10
+    );
+
+    CreditAccount accountAlex = new CreditAccount(
+            2000,
+            1000,
+            10
+    );
+    Bank bank = new Bank();
 
     @Test
     public void shouldTransfer() {
 
-        roman.add(5000);
-        alex.add(2000);
-
-        Bank bank = new Bank();
-
-        Assertions.assertTrue(bank.transfer(roman, alex, 1000));
-        Assertions.assertEquals(4000, roman.getBalance());
-        Assertions.assertEquals(3000, alex.getBalance());
+        Assertions.assertTrue(bank.transfer(accountRoman, accountAlex, 1000));
+        Assertions.assertEquals(4000, accountRoman.getBalance());
+        Assertions.assertEquals(3000, accountAlex.getBalance());
     }
 
     @Test
     public void shouldNotTransfer() {
 
-        roman.add(5000);
-        alex.add(2000);
-
-        Bank bank = new Bank();
-
-        bank.transfer(roman, alex, 10_000);
-
-        Assertions.assertFalse(bank.transfer(roman, alex, 10_000));
-        Assertions.assertEquals(5000, roman.getBalance());
-        Assertions.assertEquals(2000, alex.getBalance());
+        Assertions.assertFalse(bank.transfer(accountRoman, accountAlex, 10_000));
+        Assertions.assertEquals(5000, accountRoman.getBalance());
+        Assertions.assertEquals(2000, accountAlex.getBalance());
     }
 }
